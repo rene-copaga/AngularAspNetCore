c071f5e931fe599ea129d5346ff6a2a474db8e19
Rene Copaga <rene.copaga@gmail.com>
2020-11-24 15:08:58 -0400

    Breaking the Circular References

--- ServerApp/Controllers/ProductValuesController.cs
+++ ServerApp/Controllers/ProductValuesController.cs
@@ -19,12 +19,26 @@ namespace ServerApp.Controllers
         [HttpGet("{id}")]
         public Product GetProduct(long id)
         {
-            //System.Threading.Thread.Sleep(5000);
-            return context.Products
+            Product result = context.Products
                 .Include(p => p.Supplier)
                 .Include(p => p.Ratings)
                 .FirstOrDefault(p => p.ProductId == id);
-        }
 
+            if (result != null)
+            {
+                if (result.Supplier != null)
+                {
+                    result.Supplier.Products = null;
+                }
+                if (result.Ratings != null)
+                {
+                    foreach (Rating r in result.Ratings)
+                    {
+                        r.Product = null;
+                    }
+                }
+            }
+            return result;
+        }
     }
 }

e782c131b2acdb70bf31409339f9697b48b19533
Rene Copaga <rene.copaga@gmail.com>
2020-11-24 14:28:12 -0400

    Understanding Live Data and Asynchronous Responses

--- ClientApp/src/app/models/repository.ts
+++ ClientApp/src/app/models/repository.ts
@@ -4,7 +4,7 @@ import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class Repository {
-  product: Product;
+  productData: Product;
 
   constructor(private http: HttpClient) {
     this.getProduct(1);
@@ -12,6 +12,14 @@ export class Repository {
 
   getProduct(id: number) {
     this.http.get<Product>("/api/products/" + id)
-      .subscribe(p => this.product = p);
+      .subscribe(p => {
+        this.productData = p;
+        console.log("Product Data Received");
+      });
+  }
+
+  get product(): Product {
+    console.log("Product Data Requested");
+    return this.productData;
   }
 }

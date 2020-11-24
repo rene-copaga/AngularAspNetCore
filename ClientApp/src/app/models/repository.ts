9c5f7187cc54a687db7592282b8bd881a5c235e2
Rene Copaga <rene.copaga@gmail.com>
2020-11-24 18:08:15 -0400

    Applying Filtering in the Angular Application

--- ClientApp/src/app/models/repository.ts
+++ ClientApp/src/app/models/repository.ts
@@ -1,6 +1,7 @@
 import { Product } from "./product.model";
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Filter } from "./configClasses.repository";
 
 const productsUrl = "/api/products";
 
@@ -8,8 +9,11 @@ const productsUrl = "/api/products";
 export class Repository {
   product: Product;
   products: Product[];
+  filter: Filter = new Filter();
 
   constructor(private http: HttpClient) {
+    this.filter.category = "soccer";
+    this.filter.related = true;
     this.getProducts(true);
   }
 
@@ -19,7 +23,13 @@ export class Repository {
   }
 
   getProducts(related = false) {
-    this.http.get<Product[]>(`${productsUrl}?related=${related}`)
-      .subscribe(prods => this.products = prods);
+    let url = `${productsUrl}?related=${this.filter.related}`;
+    if (this.filter.category) {
+      url += `&category=${this.filter.category}`;
+    }
+    if (this.filter.search) {
+      url += `&search=${this.filter.search}`;
+    }
+    this.http.get<Product[]>(url).subscribe(prods => this.products = prods);
   }
 }

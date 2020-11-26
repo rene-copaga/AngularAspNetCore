af0e90e1804995f44adc5920f249708efea5c9e5
Rene Copaga <rene.copaga@gmail.com>
2020-11-26 08:30:10 -0400

    Enhancing the Product Binding Target

--- ServerApp/Models/BindingTargets/ProductData.cs
+++ ServerApp/Models/BindingTargets/ProductData.cs
@@ -5,26 +5,48 @@ namespace ServerApp.Models.BindingTargets
     public class ProductData
     {
         [Required]
-        public string Name { get; set; }
+        public string Name { 
+            get => Product.Name; set => Product.Name = value;
+        }
 
         [Required]
-        public string Category { get; set; }
+        public string Category
+        {
+            get => Product.Category; set => Product.Category = value;
+        }
 
         [Required]
-        public string Description { get; set; }
+        public string Description
+        {
+            get => Product.Description; set => Product.Description = value;
+        }
 
         [Range(1, int.MaxValue, ErrorMessage = "Price must be at least 1")]
-        public decimal Price { get; set; }
-
-        public long Supplier { get; set; }
-
-        public Product Product => new Product
+        public decimal Price
         {
-            Name = Name,
-            Category = Category,
-            Description = Description,
-            Price = Price,
-            Supplier = Supplier == 0 ? null : new Supplier { SupplierId = Supplier }
-        };
+            get => Product.Price; set => Product.Price = value;
+        }
+
+        public long? Supplier
+        {
+            get => Product.Supplier?.SupplierId ?? null;
+            set
+            {
+                if (!value.HasValue)
+                {
+                    Product.Supplier = null;
+                }
+                else
+                {
+                    if (Product.Supplier == null)
+                    {
+                        Product.Supplier = new Supplier();
+                    }
+                    Product.Supplier.SupplierId = value.Value;
+                }
+            }
+        }
+
+        public Product Product { get; set; } = new Product();
     }
 }

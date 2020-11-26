f13f15ad97d114504a26e19601cdd9fdd0e41a41
Rene Copaga <rene.copaga@gmail.com>
2020-11-26 12:06:29 -0400

    Deleting Objects in the Angular Application

--- ClientApp/src/app/models/repository.ts
+++ ClientApp/src/app/models/repository.ts
@@ -96,4 +96,17 @@ export class Repository {
     this.http.patch(`${productsUrl}/${id}`, patch)
       .subscribe(() => this.getProducts());
   }
+
+  deleteProduct(id: number) {
+    this.http.delete(`${productsUrl}/${id}`)
+      .subscribe(() => this.getProducts());
+  }
+
+  deleteSupplier(id: number) {
+    this.http.delete(`${suppliersUrl}/${id}`)
+      .subscribe(() => {
+        this.getProducts();
+        this.getSuppliers();
+      });
+  }
 }

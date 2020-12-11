a831ca7af2a04adec2e8bdfd1a4152e177e4afe2
Rene Copaga <rene.copaga@gmail.com>
2020-12-11 10:28:32 -0400

    Updating the Filter Component and Template

--- ClientApp/src/app/store/categoryFilter.component.ts
+++ ClientApp/src/app/store/categoryFilter.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { Repository } from "../models/repository";
+import { NavigationService } from '../models/navigation.service';
 
 @Component({
   selector: "store-categoryfilter",
@@ -7,5 +8,5 @@ import { Repository } from "../models/repository";
 })
 export class CategoryFilterComponent {
 
-  constructor(private repo: Repository) { }
+  constructor(private service: NavigationService) { }
 }

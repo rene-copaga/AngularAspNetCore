6641390cc44409b333226dffe5435d60a31cb39d
Rene Copaga <rene.copaga@gmail.com>
2020-12-11 10:10:53 -0400

    Updating the Angular Routing Configuration

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ProductSelectionComponent } from "./store/productSelection.component";
 
 
 const routes: Routes = [
+  { path: "store/:category", component: ProductSelectionComponent },
   { path: "store", component: ProductSelectionComponent },
   { path: "", redirectTo: "/store", pathMatch: "full" }
 ];

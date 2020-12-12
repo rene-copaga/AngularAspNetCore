a75566784cae1e3b59606170f9f3379ec38bc52a
Rene Copaga <rene.copaga@gmail.com>
2020-12-12 14:33:45 -0400

    Wiring Up the Buttons

--- ClientApp/src/app/store/productList.component.ts
+++ ClientApp/src/app/store/productList.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { Repository } from "../models/repository";
 import { Product } from "../models/product.model";
+import { Cart } from '../models/cart.model';
 
 @Component({
   selector: "store-product-list",
@@ -8,7 +9,7 @@ import { Product } from "../models/product.model";
 })
 export class ProductListComponent {
 
-  constructor(private repo: Repository) { }
+  constructor(private repo: Repository, private cart: Cart) { }
 
   get products(): Product[] {
     if (this.repo.products != null && this.repo.products.length > 0) {
@@ -17,5 +18,9 @@ export class ProductListComponent {
       return this.repo.products.slice(pageIndex,
         pageIndex + this.repo.paginationObject.productsPerPage);
     }
+  }
+
+  addToCart(product: Product) {
+    this.cart.addProduct(product);
   }
 }

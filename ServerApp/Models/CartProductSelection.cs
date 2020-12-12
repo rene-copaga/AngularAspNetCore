9af55b97fbb17904bdbc7ee5af69b9abbfa3c11b
Rene Copaga <rene.copaga@gmail.com>
2020-12-12 16:13:03 -0400

    Storing Carts Extending the ASP.NET Core MVC Data Model

---
+++ ServerApp/Models/CartProductSelection.cs
@@ -0,0 +1,10 @@
+﻿namespace ServerApp.Models
+{
+    public class CartProductSelection
+    {
+        public long productId { get; set; }
+        public string name { get; set; }
+        public decimal price { get; set; }
+        public int quantity { get; set; }
+    }
+}

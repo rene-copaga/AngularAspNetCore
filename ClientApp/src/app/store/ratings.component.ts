467bf1245a2fbbb5d2ba7905dc13825e58a2829d
Rene Copaga <rene.copaga@gmail.com>
2020-12-12 09:43:14 -0400

    Updating the Rating Component and Template

--- ClientApp/src/app/store/ratings.component.ts
+++ ClientApp/src/app/store/ratings.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { Product } from "../models/product.model";
 
 @Component({
@@ -7,4 +7,19 @@ import { Product } from "../models/product.model";
 })
 export class RatingsComponent {
 
+  @Input()
+  product: Product;
+
+  get stars(): boolean[] {
+    if (this.product != null && this.product.ratings != null) {
+      let total = this.product.ratings.map(r => r.stars)
+        .reduce((prev, curr) => prev + curr, 0);
+      let count = Math.round(total / this.product.ratings.length);
+      return Array(5).fill(false).map((value, index) => {
+        return index < count;
+      });
+    } else {
+      return [];
+    }
+  }
 }

187512542a57ddc9a70aa544116260557d028a51
Rene Copaga <rene.copaga@gmail.com>
2020-12-14 16:17:30 -0400

    Updating the Angular Application

--- ClientApp/src/app/store/store.module.ts
+++ ClientApp/src/app/store/store.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from "@angular/core";
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
 import { CartSummaryComponent } from "./cartSummary.component";
 import { CategoryFilterComponent } from "./categoryFilter.component";
@@ -13,13 +13,15 @@ import { CheckoutDetailsComponent } from './checkout/checkoutDetails.component';
 import { CheckoutPaymentComponent } from './checkout/checkoutPayment.component';
 import { CheckoutSummaryComponent } from './checkout/checkoutSummary.component';
 import { OrderConfirmationComponent } from './checkout/orderConfirmation.component';
+import { BlazorLoader } from "./blazorLoader.component";
 
 @NgModule({
   declarations: [CartSummaryComponent, CategoryFilterComponent,
     PaginationComponent, ProductListComponent, RatingsComponent,
     ProductSelectionComponent, CartDetailComponent, CheckoutDetailsComponent,
-    CheckoutPaymentComponent, CheckoutSummaryComponent, OrderConfirmationComponent],
+    CheckoutPaymentComponent, CheckoutSummaryComponent, OrderConfirmationComponent, BlazorLoader],
   imports: [BrowserModule, FormsModule, RouterModule],
-  exports: [ProductSelectionComponent]
+  exports: [ProductSelectionComponent],
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class StoreModule { }

17045ef82b2a39b3d20c5116312db0d64f5dcf99
Rene Copaga <rene.copaga@gmail.com>
2020-12-14 09:33:22 -0400

    Storing Checkout Data

--- ClientApp/src/app/models/order.model.ts
+++ ClientApp/src/app/models/order.model.ts
@@ -1,6 +1,16 @@
 import { Injectable } from "@angular/core";
 import { Cart } from "./cart.model";
 import { Repository } from "./repository";
+import { Router, NavigationStart } from "@angular/router";
+import { filter } from "rxjs/operators";
+
+type OrderSession = {
+  name: string,
+  address: string,
+  cardNumber: string,
+  cardExpiry: string,
+  cardSecurityCode: string
+}
 
 @Injectable()
 export class Order {
@@ -13,7 +23,32 @@ export class Order {
   shipped: boolean = false;
   orderConfirmation: OrderConfirmation;
 
-  constructor(private repo: Repository, public cart: Cart) { }
+  constructor(private repo: Repository, public cart: Cart, router: Router) {
+    router.events
+      .pipe(filter(event => event instanceof NavigationStart))
+      .subscribe(event => {
+        if (router.url.startsWith("/checkout")
+          && this.name != null && this.address != null) {
+          repo.storeSessionData<OrderSession>("checkout", {
+            name: this.name,
+            address: this.address,
+            cardNumber: this.payment.cardNumber,
+            cardExpiry: this.payment.cardExpiry,
+            cardSecurityCode: this.payment.cardSecurityCode
+          });
+        }
+      });
+
+    repo.getSessionData<OrderSession>("checkout").subscribe(data => {
+      if (data != null) {
+        this.name = data.name;
+        this.address = data.address;
+        this.payment.cardNumber = data.cardNumber;
+        this.payment.cardExpiry = data.cardExpiry;
+        this.payment.cardSecurityCode = data.cardSecurityCode;
+      }
+    })
+  }
 
   get products(): CartLine[] {
     return this.cart.selections

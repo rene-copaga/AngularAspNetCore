7995e229cc70c8c26107fe9c7827964f1eac9c5c
Rene Copaga <rene.copaga@gmail.com>
2020-12-14 13:25:41 -0400

    Configuring the MVC Project to Serve Blazor Content

--- ServerApp/Startup.cs
+++ ServerApp/Startup.cs
@@ -1,14 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.ResponseCompression;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using ServerApp.Models;
@@ -55,6 +58,11 @@ namespace ServerApp
                 options.Cookie.HttpOnly = false;
                 options.Cookie.IsEssential = true;
             });
+
+            services.AddResponseCompression(opts => {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                new[] { "application/octet-stream" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -74,6 +82,14 @@ namespace ServerApp
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                RequestPath = "/blazor",
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(),
+                    "../BlazorApp/wwwroot"))
+            });
+
             app.UseSession();
 
             app.UseRouting();
@@ -91,8 +107,14 @@ namespace ServerApp
                     pattern: "{target:regex(store|cart|checkout)}/{*catchall}",
                     defaults: new { controller = "Home", action = "Index" });
 
+                endpoints.MapFallbackToClientSideBlazor<BlazorApp
+                    .Startup>("blazor/{*path:nonfile}", "index.html");
+
                 endpoints.MapRazorPages();
             });
+
+            app.Map("/blazor", opts => 
+                opts.UseClientSideBlazorFiles<BlazorApp.Startup>());
 
             app.UseSwagger();
             app.UseSwaggerUI(options => {

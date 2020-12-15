65fa8c598e5cceb9e9ce27dc30b840277b0d6c86
Rene Copaga <rene.copaga@gmail.com>
2020-12-15 08:39:41 -0400

    Administering Products Creating the Editor Component

--- ClientApp/src/app/admin/admin.module.ts
+++ ClientApp/src/app/admin/admin.module.ts
@@ -5,6 +5,8 @@ import { AdminComponent } from "./admin.component";
 import { OverviewComponent } from "./overview.component";
 import { ProductAdminComponent } from "./productAdmin.component";
 import { OrderAdminComponent } from "./orderAdmin.component";
+import { CommonModule } from '@angular/common';
+import { ProductEditorComponent } from './productEditor.component';
 
 const routes: Routes = [
   {
@@ -19,8 +21,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule,
-    FormsModule, RouterModule.forChild(routes)],
+    FormsModule, RouterModule.forChild(routes), CommonModule],
   declarations: [AdminComponent, OverviewComponent,
-    ProductAdminComponent, OrderAdminComponent]
+    ProductAdminComponent, OrderAdminComponent, ProductEditorComponent]
 })
 export class AdminModule { }

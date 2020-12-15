508cffb5e0d7f897ae2f4b4787fcc1c8df4fec46
Rene Copaga <rene.copaga@gmail.com>
2020-12-15 08:17:49 -0400

    Creating the Feature Module and Routing Configuration

---
+++ ClientApp/src/app/admin/admin.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+import { AdminComponent } from "./admin.component";
+import { OverviewComponent } from "./overview.component";
+import { ProductAdminComponent } from "./productAdmin.component";
+import { OrderAdminComponent } from "./orderAdmin.component";
+
+const routes: Routes = [
+  {
+    path: "", component: AdminComponent,
+    children: [
+      { path: "products", component: ProductAdminComponent },
+      { path: "orders", component: OrderAdminComponent },
+      { path: "overview", component: OverviewComponent },
+      { path: "", component: OverviewComponent }]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule,
+    FormsModule, RouterModule.forChild(routes)],
+  declarations: [AdminComponent, OverviewComponent,
+    ProductAdminComponent, OrderAdminComponent]
+})
+export class AdminModule { }
